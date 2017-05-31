2caec54a849349dd741d12bf277ae313b55f164c
Streed12 <steve.t.reed@gmail.com>
2017-05-31 12:36:37 -0700

    Testing functionality.

--- index.js
+++ index.js
@@ -11,13 +11,19 @@ app.listen(process.env.PORT || 5000);
 // Config.keys uses environment variables so sensitive info is not in the repo.
 var config = {
     me: 'FishMojiApp', // The authorized account with a list to retweet.
-    myList: 'Bass-pros-industry', // The list we want to retweet.
+    list: 'BassAnglers', // The list we want to retweet.
     regexFilter: '', // Accept only tweets matching this regex pattern.
     regexReject: '(@)', // AND reject any tweets matching this regex pattern.
 
 
+    // keys: {
+    //     consumer_key: 'IpbOqTEAxBuZWjMgHMVrKGRhb',
+    //     consumer_secret: 'ffwKdzsvUGMmqHLM8EXrEEuurbLkZJFY8wOAcLRWe2GgtZWqPI',
+    //     access_token_key: '710987248464994305-ELnaxhD7nBLWxmoMECWOpPrFNRv7nLK',
+    //     access_token_secret: 'WfqfWsGatD3AO9y30xuWX1GG6pLToH2gCOzHaIdxYxP2x'
+    // },
     keys: {
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_key: '9SoZMVWpNArCsEwFxXZmtOrSO' process.env.TWITTER_CONSUMER_KEY,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
         access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
@@ -27,19 +33,23 @@ var config = {
 
 // Get the members of our list, and pass them into a callback function.
 function getListMembers(callback) {
+    let bassMembers = new Map();
+    bassMembers.clear();
     var memberIDs = [];
 
     tu.listMembers({owner_screen_name: config.me,
-        slug: config.myList
+        slug: config.list
     },
+
     function(error, data){
         if (!error) {
-            for (var i=0; i < data.users.length; i++) {
+            for (var i = 0; i < data.users.length; i++) {
+                bassMembers.set(data.users[i].id_str, data.users[i].screen_name);
                 memberIDs.push(data.users[i].id_str);
             }
 
             // This callback is designed to run listen(memberIDs).
-            callback(memberIDs);
+            callback(memberIDs, bassMembers);
         } else {
             console.log(error);
             console.log(data);
@@ -67,35 +77,44 @@ function doFavorite(err) {
 }
 
 // What to do when we get a tweet.
-// function onTweet(tweet) {
-//     console.log('favorite', tweet)
-//     // Reject the tweet if:
-//     //  1. it's flagged as a retweet
-//     //  2. it matches our regex rejection criteria
-//     //  3. it doesn't match our regex acceptance filter
-//     var regexReject = new RegExp(config.regexReject, 'i');
-//     var regexFilter = new RegExp(config.regexFilter, 'i');
-//     if (tweet.retweeted) {
-//         return;
-//     }
-//     if (config.regexReject !== '' && regexReject.test(tweet.text)) {
-//         return;
-//     }
-//     if (regexFilter.test(tweet.text)) {
-//         console.log(tweet);
-//         if(tweet.filter_level === 'low' &&
-//            !tweet.in_reply_to_status_id &&
-//            !tweet.in_reply_to_user_id &&
-//             tweet.lang === 'en') {
-//               tu.createFavorite({
-//                     id: tweet.id_str
-//               }, doFavorite);
-//         }
-//     }
-// }
+function onTweet(tweet, fishMembers) {
+    // Reject the tweet if:
+    //  1. it's flagged as a retweet
+    //  2. it matches our regex rejection criteria
+    //  3. it doesn't match our regex acceptance filter
+    var regexReject = new RegExp(config.regexReject, 'i');
+    var regexFilter = new RegExp(config.regexFilter, 'i');
+    if (tweet.retweeted) {
+        return;
+    }
+    if (config.regexReject !== '' && regexReject.test(tweet.text)) {
+        return;
+    }
+    if (regexFilter.test(tweet.text)) {
+        if(tweet.filter_level === 'low' &&
+           tweet.in_reply_to_status_id === null &&
+           tweet.in_reply_to_user_id === null &&
+            tweet.lang === 'en') {
+            if(fishMembers.has(tweet.user.id_str)){
+                console.log(tweet)
+                tu.retweet({
+                    id: tweet.id_str
+                }, onReTweet)
+            } else {
+                console.log('fave')
+                tu.createFavorite({
+                    id: tweet.id_str
+              }, doFavorite);
+            }
+              // tu.createFavorite({
+              //       id: tweet.id_str
+              // }, doFavorite);
+              
+        }
+    }
+}
 
 function onListTweet(tweet) {
-    console.log('Retweet', tweet)
     // Reject the tweet if:
     //  1. it's flagged as a retweet
     //  2. it matches our regex rejection criteria
@@ -123,22 +142,26 @@ function onListTweet(tweet) {
 }
 
 // Function for listening to twitter streams and retweeting on demand.
-function listen(listMembers) {
+function listen(listMembers, usersList) {
     tu.filter({
-        follow: listMembers
+        follow: listMembers,
+        track: '#fishing, #bassfishing'
     }, function(stream) {
         console.log("listening to stream")
-        stream.on('tweet', onListTweet)
+        stream.on('tweet', (tweet) => {
+            onTweet(tweet, usersList)
+        });
     });
 
     // tu.filter({
-    //     track: 'bassfishing, swimbait, bassmaster, FLWfishing'
+    //     track: 'fishing, bassfishing'
     // }, function(stream) {
     //     console.log("listening to stream");
     //     stream.on('tweet', onTweet);
     // });
     
 }
+
 //Can add more filters / watchers here.
 
 
