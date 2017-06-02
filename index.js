1558f4c677daad0dd3d32fc9d6549c0efc2a4cfc
Streed12 <steve.t.reed@gmail.com>
2017-06-01 18:59:25 -0700

    Restructuring files.

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
 // START HEROKU SETUP
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
+//Twitter bot config - store in separate file for cleaner main code
+const { config, trackTerms } = require('./config.js')
+
 app.get('/', function(req, res){ res.send('The robot is happily running.'); });
 app.listen(process.env.PORT || 5000);
-// END HEROKU SETUP
-
-
-// Listbot config
-//
-// Config.keys uses environment variables so sensitive info is not in the repo.
-let config = {
-  me: 'FishMojiApp', // The authorized account with a list to retweet.
-  list: 'Bass-SM', // The list we want to retweet.
-  regexFilter: '', // Accept only tweets matching this regex pattern.
-  regexReject: '(@|music|guitar|sex|viral|follow)', // AND reject any tweets matching this regex pattern.
-
-  keys: {
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  },
-};
-
 
 // Get the members of our list, and pass them into a callback function.
 const getListMembers = (callback) => {
-  let bassMembers = new Map();
-  bassMembers.clear();
-
+  let listMembers = new Map();
+  listMembers.clear();
   let memberIDs = [];
 
   tu.listMembers({owner_screen_name: config.me,
-      slug: config.list
+      slug: config.list,
+      count: 500
   }, (error, data) => {
       if (!error) {
         for (let i = 0; i < data.users.length; i++) {
-          bassMembers.set(data.users[i].id_str, data.users[i].screen_name);
+          listMembers.set(data.users[i].id_str, data.users[i].screen_name);
           memberIDs.push(data.users[i].id_str);
         }
-
         // This callback is designed to run listen(memberIDs).
-        callback(memberIDs, bassMembers);
+        callback(memberIDs, listMembers);
       } else {
         console.log(error);
         console.log(data);
@@ -58,6 +40,7 @@ const onReTweet = (err) => {
       console.log('success retweet')
   }
 }
+// What to do after we favorite something.
 const doFavorite = (err) => {
   if(err) {
       console.error("Favorite failed :(");
@@ -69,52 +52,53 @@ const doFavorite = (err) => {
 }
 
 // What to do when we get a tweet.
-const onTweet = (tweet, fishMembers) => {
-    // Reject the tweet if:
-    //  1. it's flagged as a retweet
-    //  2. it matches our regex rejection criteria
-    //  3. it doesn't match our regex acceptance filter
+const onTweet = (tweet, listMembers) => {
+  console.log(tweet)
+  //Destructure the tweet info we want
+  const { id_str, user, text } = tweet;
+  //Destructure the tweet properties we want to check as filters
+  const { in_reply_to_status_id: inReply, in_reply_to_user_id: toUser, retweeted, filter_level, lang } = tweet;
+
+  // Reject the tweet if:
+  //  1. it's flagged as a retweet
+  //  2. it matches our regex rejection criteria
+  //  3. it doesn't match our regex acceptance filter
   let regexReject = new RegExp(config.regexReject, 'i');
   let regexFilter = new RegExp(config.regexFilter, 'i');
-  if (tweet.retweeted) {
+  if (retweeted || inReply || replyUser || lang !== 'en' || filter_level !== 'low') {
       return;
   }
   if (config.regexReject !== '' && regexReject.test(tweet.text)) {
       return;
   }
-  if (regexFilter.test(tweet.text)) {
-    if(tweet.filter_level === 'low' &&
-      tweet.in_reply_to_status_id === null &&
-      tweet.in_reply_to_user_id === null &&
-      tweet.lang === 'en') {
-        if(fishMembers.has(tweet.user.id_str)){
-          tu.retweet({
-            id: tweet.id_str
-          }, onReTweet)
-          
-        } else {
-          tu.createFavorite({
-            id: tweet.id_str
-          }, doFavorite);
-          
-          tu.createFriendship({
-            id: tweet.user.id_str,
-            follow: true
-          }, doFavorite)
-        }
+  if (regexFilter.test(text)) {
+    if(listMembers.has(user.id_str)){
+      tu.retweet({
+        id: id_str
+      }, onReTweet)
+      
+    } else {
+      tu.createFavorite({
+        id: id_str
+      }, doFavorite);
+      
+      tu.createFriendship({
+        id: user.id_str,
+        follow: true
+      }, doFavorite)
     }
   }
 }
 
 // Function for listening to twitter streams and retweeting on demand.
-const listen = (listMembers, usersList) => {
+const listen = (memberIDs, listMembers) => {
   tu.filter({
-    follow: listMembers,
-    track: ['#bassfishing', '#basspro', '#bassmaster', '#flwfishing', '#tacklewarehouse', '#swimbait']
+    follow: memberIDs,
+    track: trackTerms
   }, function(stream) {
       console.log("listening to stream")
       stream.on('tweet', (tweet) => {
-          onTweet(tweet, usersList)
+          onTweet(tweet, listMembers)
       });
   });
 }
