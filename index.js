f221e2b07a959659a38d312b0ad9eead7811d0c9
Streed12 <steve.t.reed@gmail.com>
2017-06-02 13:52:18 -0700

    Working on blocked users.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // START HEROKU SETUP
-const express = require("express");
+const express = require('express');
 const app = express();
+const _ = require('lodash');
 //Twitter bot config - store in separate file for cleaner main code
 const { config, trackTerms } = require('./config.js')
 
@@ -11,7 +12,14 @@ app.listen(process.env.PORT || 5000);
 const getListMembers = (callback) => {
   let listMembers = new Map();
   listMembers.clear();
-  let memberIDs = [];
+  let memberIDs = {
+    blocked: [],
+    following: []
+  };
+
+  tu.blocksList({owner_screen_name: config.me}, (error, data) => {
+    data.users.forEach(bUser => memberIDs.blocked.push(bUser.id_str))
+  })
 
   tu.listMembers({owner_screen_name: config.me,
       slug: config.list,
@@ -20,7 +28,7 @@ const getListMembers = (callback) => {
       if (!error) {
         for (let i = 0; i < data.users.length; i++) {
           listMembers.set(data.users[i].id_str, data.users[i].screen_name);
-          memberIDs.push(data.users[i].id_str);
+          memberIDs.following.push(data.users[i].id_str);
         }
         // This callback is designed to run listen(memberIDs).
         callback(memberIDs, listMembers);
@@ -29,6 +37,7 @@ const getListMembers = (callback) => {
         console.log(data);
       }
   });
+  
 }
 
 // What to do after we retweet something.
@@ -52,24 +61,32 @@ const doFavorite = (err) => {
 }
 
 // What to do when we get a tweet.
-const onTweet = (tweet, listMembers) => {
+const onTweet = (tweet, blocked, listMembers) => {
+
   //Destructure the tweet info we want
   const { id_str, user, text } = tweet;
   //Destructure the tweet properties we want to check as filters
   const { in_reply_to_status_id: inReply, in_reply_to_user_id: replyUser, retweeted, filter_level, lang } = tweet;
-
+  console.log(blocked, user.id_str, _.includes(blocked, user.id_str), blocked.length)
   // Reject the tweet if:
   //  1. it's flagged as a retweet
   //  2. it matches our regex rejection criteria
   //  3. it doesn't match our regex acceptance filter
   let regexReject = new RegExp(config.regexReject, 'i');
   let regexFilter = new RegExp(config.regexFilter, 'i');
+  
+
+  if(_.includes(blocked, user.id_str)){
+    console.log('blocked', user.screen_name)
+    return;
+  }
   if (retweeted || inReply || replyUser || lang !== 'en' || filter_level !== 'low') {
       return;
   }
   if (config.regexReject !== '' && regexReject.test(tweet.text)) {
       return;
   }
+
   if (regexFilter.test(text)) {
     if(listMembers.has(user.id_str)){
       tu.retweet({
@@ -80,24 +97,25 @@ const onTweet = (tweet, listMembers) => {
       tu.createFavorite({
         id: id_str
       }, doFavorite);
-      
+
       tu.createFriendship({
         id: user.id_str,
         follow: true
       }, doFavorite)
-    }
-  }
+    }   
+  } 
 }
 
 // Function for listening to twitter streams and retweeting on demand.
 const listen = (memberIDs, listMembers) => {
+  let { blocked, following } = memberIDs
   tu.filter({
-    follow: memberIDs,
+    follow: memberIDs.following,
     track: trackTerms
   }, function(stream) {
       console.log("listening to stream")
       stream.on('tweet', (tweet) => {
-          onTweet(tweet, listMembers)
+          onTweet(tweet, blocked, listMembers)
       });
   });
 }
